e314c8228f747e4399eb967d7830a52356118283
minhng <minhng@hypersol.vn>
2023-12-15 11:23:25 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/test_deploy_vite/",
   plugins: [vue()],
-  define: { 'process.env' : {
-    VITE_API : process.env.VITE_API
-  }},
+  define: {
+    'process.env': {
+      VITE_API: process.env.VITE_API
+    }
+  },
   resolve: {
     alias: {
       '@': '/src',
